052a16ae9a7115ccbe9a61a94ef5ac16f99642ab
malenaHuilcaleo <huilcaleo.sofia@gmail.com>
2023-05-25 12:42:44 -0300

    modulo colores

--- moduloColores.java
+++ moduloColores.java
@@ -1,8 +1,21 @@
 public class moduloColores {
     public static void main(String[] args) {
-        
+        String colorBurbuja1;
+        colorBurbuja1 = colorAleatorio();
+        System.out.println(colorBurbuja1);
+        System.out.println();
 
+}
 
+public static String colorAleatorio(){
+    String color [] = {"Rojo", "Celeste"};
+    return color[generarNumAleatorio(0, color.length -1)];
+}
+
+public static int generarNumAleatorio(int min, int max){
+    int numero=0;
+    numero= (int)(Math.random()*(max-min)+min);//genera un numero aleatorio en un rango de diez numeros comenzando desde 1
+    return numero;
 }
   
 }
